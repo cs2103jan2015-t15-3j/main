f09d8615878f15d517a1fb2558852d9c58627491
Ash <mdashmawi3001@gmail.com>
2015-03-04 18:17:22 +0800

    added skeleton methods

--- src/MainFunc.java
+++ src/MainFunc.java
@@ -1,7 +1,21 @@
+import java.util.*;
+
 
 public class MainFunc {
+public static ArrayList<String> myList;
+public static ArrayList<String> undoList;
+private static final String MESSAGE_TASK_ADDED = "Task has been added.\n";
 
+	public String addTask(String taskName) {
+		myList.add(taskName);
+		return MESSAGE_TASK_ADDED;		
+	}
 	
+	public void deleteTask(String taskName) {}
 	
+	public void searchTask(String taskName) {}
 	
+	public void sortTask(String taskName) {}
+	
+	public void POWERsearchTask(String taskName) {}
 }
