a29b733377b04659c626129593c5aca81049f54e
Ash <mdashmawi3001@gmail.com>
2015-03-09 15:38:18 +0800

    Parser Class added, still in progress

---
+++ src/Parser.java
@@ -0,0 +1,32 @@
+import java.util.*;
+import java.io.*; 
+
+public class Parser {
+	
+	public void receiveInput() {
+		
+		date();
+		text();
+		store();
+	}
+	
+	public void date() {
+		if(isDate()) {
+			
+		}
+	}
+	
+	public boolean isDate(){
+		return true;
+	}
+	
+	public void text() {
+		String textContent = new String();
+	}
+	
+	public void store() {
+		
+	}
+	
+	
+}
