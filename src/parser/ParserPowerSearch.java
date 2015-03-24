825ca4187afd04699eea0b8c3b0d186ae9a78c29
Samson <samson.g@live.com>
2015-03-24 13:23:28 +0800

    Revert "Updated version"
    This reverts commit ee7c2eb634f7a59e466dcabf33f0baa8472e0c6e.

--- src/parser/ParserPowerSearch.java
+++
@@ -1,7 +0,0 @@
-package parser;
-
-public class ParserPowerSearch {
-	public static void powerSearchTask(Interpreter item, String[] inputArray) {
-		
-	}
-}
--- src/parser/ParserUncompleteTask.java
+++
@@ -1,9 +0,0 @@
-package parser;
-
-public class ParserUncompleteTask {
-	public static void uncompleteTask(Interpreter item, String[] inputArray) {
-		int ID = Integer.parseInt(inputArray[1]);
-		item.setTaskID(ID);
-		item.setIsCompleted(false);
-	}
-}
