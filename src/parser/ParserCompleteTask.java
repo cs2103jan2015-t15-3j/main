e6d03be252662a0d99b586499a14d592d4368064
Samson <samson.g@live.com>
2015-03-24 13:29:15 +0800

    Updated

---
+++ src/parser/ParserCompleteTask.java
@@ -0,0 +1,11 @@
+package parser;
+
+public class ParserCompleteTask {
+
+	public static void completeTask(Interpreter item, String[] inputArray) {
+		int ID = Integer.parseInt(inputArray[1]);
+		item.setTaskID(ID);
+		item.setIsCompleted(true);
+	}
+	
+}
---
+++ src/parser/ParserDelete.java
@@ -0,0 +1,11 @@
+package parser;
+
+public class ParserDelete {
+
+	public static void deleteTask(Interpreter item, String[] inputArray) {
+		int ID = Integer.parseInt(inputArray[1]);
+		item.setTaskID(ID);
+	}
+	
+	
+}
