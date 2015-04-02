1031abdf0ec7fbf9c307482afa6b3524dcbcd1f4
laulijun <laulijunn@gmail.com>
2015-04-02 16:00:14 +0800

    uncomment initial loading

--- src/userInterface/UserInterfaceMain.java
+++ src/userInterface/UserInterfaceMain.java
@@ -86,17 +86,18 @@ public class UserInterfaceMain extends JPanel {
 						.getResource("/userInterface/ImageIcon/toDoIcon.png")));
 
 		
-//		try{
-//			mem = LogicMain.getAllTasks(mem);
-//			toDoPanel.revalidate();
-//			toDoPanel.repaint();
-//			toDoPanel.removeAll();
-//			printLabel(mem);
-//		}
-//		
-//		catch(FileNotFoundException e1){
-//			
-//		}
+		try{
+			mem = LogicMain.getAllTasks();
+			toDoPanel.revalidate();
+			toDoPanel.repaint();
+			toDoPanel.removeAll();
+			printLabel(mem);
+		}
+		
+		catch(FileNotFoundException e1){
+			
+		}
+		
 		JScrollPane toDoScroller = new JScrollPane();
 		toDoScroller.setVisible(true);
 
