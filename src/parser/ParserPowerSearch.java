35e2854b08038025ea009900e7e43af60324b29d
Ash <mdashmawi3001@gmail.com>
2015-04-11 18:11:52 +0800

    PowerSearch edited

--- src/parser/ParserPowerSearch.java
+++ src/parser/ParserPowerSearch.java
@@ -2,27 +2,29 @@ package parser;
 
 import java.util.ArrayList;
 
-import storage.KeyWord;
 import storage.KeyWordStorage;
 import logic.Task;
 
 public class ParserPowerSearch {
 	
+	private static int START_INDEX_CASE_PS = 3;
+	private static int START_INDEX_CASE_PSEARCH = 8;
+	
+	
 	public static ArrayList<Task> powerSearch(String input) {
 		String keyWords, checkCmd;
-		KeyWordStorage storage = new KeyWordStorage(input);
+		KeyWordStorage storage = new KeyWordStorage("test.csv");
 		ArrayList<Task> keyWordsList = new ArrayList<Task>();
-		ArrayList<KeyWord> keyWordsList2 = new ArrayList<KeyWord>();
 		
 		checkCmd = input.substring(0,2);
 		
 		switch(checkCmd) {
 		case "ps ":
-			keyWords = input.substring(3, input.length() - 1);
-			keyWordsList2 =  storage.powerSearch(keyWords);
+			keyWords = input.substring(START_INDEX_CASE_PS, input.length() - 1);
+			keyWordsList =  storage.powerSearch(keyWords);
 		case "pse":
-			keyWords = input.substring(8, input.length() - 1);
-			keyWordsList2 =  storage.powerSearch(keyWords);
+			keyWords = input.substring(START_INDEX_CASE_PSEARCH, input.length() - 1);
+			keyWordsList =  storage.powerSearch(keyWords);
 		}
 		return keyWordsList;
 	}
