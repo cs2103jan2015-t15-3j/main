88e33cc2e7cc83e0e03f891dc747ef6405afb902
Samson <samson.g@live.com>
2015-04-11 20:46:33 +0800

    edited Parser Message.

--- src/parser/ParserMessage.java
+++ src/parser/ParserMessage.java
@@ -1,11 +1,10 @@
 package parser;
 
 public class ParserMessage {
-	
-	protected static final String INVALID_COMMAND = "The command doesn't exist.";
-	protected static final String INVALID_INPUT = "The input format is invalid.";
-	protected static final String INVALID_TEXT  = "The text format is invalid.";
-	protected static final String INVALID_DATE_TIME_FORMAT = "The date and time format is invalid.";
-	protected static final String INVALID_ID = "The ID format is invalid.";
-			
+	protected static final String INVALID_COMMAND = "The command does not exists.";
+	protected static final String INVALID_INPUT = "The input format is you entered invalid.";
+	protected static final String INVALID_TEXT = "The text format you entered is invalid.";
+	protected static final String INVALID_DATE_TIME_FORMAT = "The date and time format you entered is invalid.";
+	protected static final String INVALID_CATEGORY = "The category format you entered is invalid.";
+	protected static final String INVALID_ID = "The ID format you entered is invalid.";
 }
