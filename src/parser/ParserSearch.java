cc63f3b4f7bd38f64050d15dbd07ec892ce06519
Ash <mdashmawi3001@gmail.com>
2015-04-11 20:45:40 +0800

    implemented exception handling

--- src/parser/ParserSearch.java
+++ src/parser/ParserSearch.java
@@ -4,14 +4,19 @@ public class ParserSearch {
 
 	public static void searchTask(Interpreter item, String[] inputArray) {
 		String searchKey = "";
-		if(inputArray.length == 1) {
-			item.setKey(null);
-		} else {
-			int lengthSearchKey = inputArray.length-1;
-			for(int i=1; i<=lengthSearchKey; i++){
-				searchKey = searchKey.concat(inputArray[i]);
+		try {
+			if(inputArray.length == 1) {
+				throw new NullPointerException();
+			} else {
+				int lengthSearchKey = inputArray.length-1;
+				for(int i=1; i<=lengthSearchKey; i++){
+					searchKey = searchKey.concat(inputArray[i]);
+				}
+				item.setKey(searchKey);
 			}
-			item.setKey(searchKey);
-		}
+		} catch (NullPointerException npe) {
+			item.setIsError(true);
+			item.setFeedbackMsg(ParserMessage.INVALID_INPUT);
+		}	
 	}
 }
