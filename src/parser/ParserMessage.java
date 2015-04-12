c5c10aa68d57782cf14903b3ee5b6ac9903f8767
Samson <samson.g@live.com>
2015-04-12 19:51:54 +0800

    Edited parser message for invalid file path

--- src/parser/ParserMessage.java
+++ src/parser/ParserMessage.java
@@ -7,4 +7,5 @@ public class ParserMessage {
 	protected static final String INVALID_DATE_TIME_FORMAT = "The date and time format you entered is invalid.";
 	protected static final String INVALID_CATEGORY = "The category format you entered is invalid.";
 	protected static final String INVALID_ID = "The ID format you entered is invalid.";
+	protected static final String INVALID_FILEPATH = "The file path does not exist.";
 }
