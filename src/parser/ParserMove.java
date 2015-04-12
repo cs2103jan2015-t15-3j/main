5454effa9923205b8cb344527f0ebe5658e11543
Muhammad Nuruddin Bin Mazlan <nuruddinmazlan@gmail.com>
2015-04-12 19:57:43 +0800

    Corrected method ParserMove

--- src/parser/ParserMove.java
+++ src/parser/ParserMove.java
@@ -1,6 +1,8 @@
 //@author A0111842R
 package parser;
 
+import java.io.File;
+
 public class ParserMove {
 
 	public static void moveDataBase(Interpreter item, String[] inputArray) {
@@ -12,16 +14,25 @@ public class ParserMove {
 			if (expectedParameter && !inputArray[i].isEmpty()) {
 				wrongFormat = true;
 				item.setIsError(true);
+				item.setFeedbackMsg(ParserMessage.INVALID_INPUT);
+				break;
+				
 			} else if (i != 0 && !inputArray[i].isEmpty() && !expectedParameter) {
 				expectedParameter = true;
 				dataBasePath = inputArray[i];
+
+				File tempPath = new File(dataBasePath);
+				if(!tempPath.exists())
+				{
+					item.setIsError(true);
+					item.setFeedbackMsg(ParserMessage.INVALID_FILEPATH);
+				}
+
+				
 			}
 		}
-		if (wrongFormat) {
-			item.setIsError(true);
-			item.setFeedbackMsg(ParserMessage.INVALID_INPUT);
-		} else {
+		if (!wrongFormat) {
 			item.setDataBasePath(dataBasePath);
-		}
+		} 
 	}
 }
