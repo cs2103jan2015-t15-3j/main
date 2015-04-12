d5e9e7a6030ec6ba566531bfdc97dbe834becc3e
Muhammad Nuruddin Bin Mazlan <nuruddinmazlan@gmail.com>
2015-04-12 19:10:17 +0800

    Corrected method for PowerSearch

--- src/parser/ParserPowerSearch.java
+++ src/parser/ParserPowerSearch.java
@@ -7,24 +7,20 @@ import logic.Task;
 
 public class ParserPowerSearch {
 	
-	private static int START_INDEX_CASE_PS = 2;
-	private static int START_INDEX_CASE_PSEARCH = 7;
+	private static final String commandPS = "ps";
+	private static int START_INDEX_CASE_PS = commandPS.length();
 	
 	
 	public static ArrayList<Task> powerSearch(String input) {
 		String keyWords, checkCmd;
-		KeyWordStorage storage = new KeyWordStorage("test.csv");
+		KeyWordStorage storage = new KeyWordStorage();
 		ArrayList<Task> keyWordsList = new ArrayList<Task>();
 		
 		checkCmd = input.substring(0,2);
 		
 		switch(checkCmd) {
-		case "ps":
+		case commandPS:
 			keyWords = input.substring(START_INDEX_CASE_PS, input.length());
-			keyWordsList =  storage.powerSearch(keyWords);
-			break;
-		case "pse":
-			keyWords = input.substring(START_INDEX_CASE_PSEARCH, input.length());
 			keyWordsList =  storage.powerSearch(keyWords);
 			break;
 		}
